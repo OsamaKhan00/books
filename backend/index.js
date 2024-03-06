815bf0db760ef3ab89a35ab69edd02a97878a305
OsamaKhan00 <97848125+OsamaKhan00@users.noreply.github.com>
2024-03-06 12:32:50 -0800

    updates

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import mysql from "mysql"
 
 
-const app = express()
+const app = express()                   // connect this to line 13
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -10,9 +10,44 @@ const db = mysql.createConnection({
     password: "Hpprobook450corei5.",
     database: "test"
 })
-app.get("/", (req,res)=>{
+
+app.use(express.json()) //allows us to use express to send json file using any clie3nt. 
+
+
+app.get("/", (req,res)=>{                   // API requests using express server......(This is a request here)
     res.json("hello this is the backend")
 });
+
+app.get("/books", (req,res)=>{
+    const q = "SELECT * FROM books"
+    db.query(q,(err,data)=>{
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+// creating a book. 
+
+//app.post("/books", (req,res)=>{
+  //  const q = "INSERT INTO books (`titlle`, `desc`,`cover`) VALUES (?)"
+    //const values = ["title from backend", 
+      //              "desc from backend", 
+        //            "cover from backend"] // array
+
+        // if we are taking data from the user and updating it in the databse we can use the following 
+app.post("/books", (req,res)=>{
+    const q = "INSERT INTO books (`titlle`, `desc`,`cover`) VALUES (?)"
+    const values = [
+        req.body.tittle,
+        req.body.desc,
+        req.body.cover
+    ] 
+
+    db.query(q, [values], (err,data)=>{
+        if(err) return  res.json(err)
+        return res.json("Book has been created")
+    })
+})
+
 app.listen(8800, ()=>{
     console.log("connected to backend!")
 })
