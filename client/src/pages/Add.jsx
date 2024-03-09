5de2df9d9ea14bd80e84145c03bb0108af80e3b4
OsamaKhan00 <97848125+OsamaKhan00@users.noreply.github.com>
2024-03-08 19:24:34 -0800

    updates

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const Add = () => {
-    const [book, setBook] = useState({ 
+    const [book,setBook] = useState({ 
         title: "",
         desc: "",
         price: null,
